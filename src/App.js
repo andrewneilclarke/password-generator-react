c3e349492aacfc3076f8baecb75e06114c60954e
andrewneilclarke <andrewneilclarke@gmail.com>
2021-12-02 20:00:31 +0100

    added responsive mobile design

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import Switch from "react-switch"
+import { CopyToClipboard } from 'react-copy-to-clipboard';
 import './App.css';
 
 function App() {
@@ -9,9 +10,7 @@ function App() {
   const [numbers, setNumbers] = useState(false)
   const [symbols, setSymbols] = useState(false)
   const [cases, setCases] = useState(false)
-  const changeLength = (e) => {
-    setLength(e.target.value)
-  }
+  const [copied, setCopied] = useState(false)
 
   const getRandomLower = () => {
     return String.fromCharCode(Math.floor(Math.random() * 26) + 97)
@@ -35,11 +34,23 @@ function App() {
     symbols: getRandomSymbol
   }
 
+  const incrementLength = () => {
+    if (length < 16) {
+      setLength(length + 1)
+    }
+  }
+  const decrementLength = () => {
+    if (length > 6) {
+      setLength(length - 1)
+    }
+  }
+
   // console.log(getRandomLower(), getRandomNumber(), getRandomSymbol(), getRandomUpper())
 
   const generatePassword = () => {
+    copied && setCopied(false)
     let pass = '';
-    const typesCount = lower + cases + numbers + symbols;
+    // const typesCount = lower + cases + numbers + symbols;
     let options = [{ lower }, { cases }, { numbers }, { symbols }].filter(item => Object.values(item)[0]);
 
     // for (let i = 0; i < length; i += typesCount) {
@@ -59,35 +70,43 @@ function App() {
     setPassword(pass)
   }
 
-  const copyToClip = () => {
-    alert(`${password} copied to clipboard`)
-    navigator.clipboard.writeText(password)
-  }
 
   return (
-    <div className="container">
-      <h3 className="title">AC Password Generator</h3>
+    <div className="app">
+      <h3 className="title">AC Password Creator</h3>
       <div className="content">
-        <div className="section length">Password Length
-          <div className="slidecontainer">
-            <input onChange={changeLength} type="range" min="8" max="20" value={length} />
+        <div className="section length">
+          <h4>Password Length</h4>
+          <div className="length-buttons">
+            <button onClick={decrementLength}>-</button>
+            <button onClick={incrementLength}>+</button>
           </div>
-          {length}
+          <p>{length}</p>
         </div>
-        <div className="section">Uppercase
+
+
+        <div className="section">
+          <p>Uppercase</p>
+
           <Switch onChange={() => setCases(!cases)} checked={cases} onColor="#0A4B80" />
         </div>
-        <div className="section">Numbers
+        <div className="section">
+          <p>Numbers</p>
           <Switch onChange={() => setNumbers(!numbers)} checked={numbers} onColor="#0A4B80" />
         </div>
-        <div className="section">Symbols
+        <div className="section">
+          <p>Symbols</p>
           <Switch onChange={() => setSymbols(!symbols)} checked={symbols} onColor="#0A4B80" />
         </div>
       </div>
       <button onClick={generatePassword}>Create</button>
       <p className="password">{password}</p>
-      <button onClick={copyToClip}>Copy</button>
-    </div>
+      {/* <button onClick={copyToClip}>Copy</button> */}
+      <CopyToClipboard text={password} onCopy={() => setCopied(true)}>
+        <button className="copy" onCopy={() => setCopied(true)}>Copy</button>
+      </CopyToClipboard>
+      {copied && <p style={{ fontSize: 'small', marginTop: '1em' }}>Copied!</p>}
+    </div >
   );
 }
 export default App;
